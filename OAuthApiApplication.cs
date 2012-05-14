2eef93027e45d39d5d79b40423841babbe065822
John Reynolds <john@thejohnreynolds.com>
2012-05-14 08:48:18 -0700

    Initial commit, not building

---
+++ OAuthApiApplication.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApi.OAuth2
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Web;
+    using System.Web.Http;
+    
+    /// <summary>
+    /// For APIs that are both resource servers and authorization servers
+    /// The Global.asax.cs of your web application should extend this base class
+    /// </summary>
+    public class OAuthApiApplication : HttpApplication
+    {
+
+
+        protected void Application_Start()
+        {
+            // Add the OAuth Authorization handler to validate all requests
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new OAuthAuthorizationHandler());
+        }
+    }
+}
---
+++ ResourceServer/OAuthAuthorizationHandler.cs
@@ -0,0 +1,108 @@
+﻿namespace WebApi.OAuth2
+{
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.IdentityModel.Policy;
+    using System.Net.Http;
+    using System.Security.Principal;
+    using DotNetOpenAuth.Messaging;
+    using DotNetOpenAuth.OAuth2;
+using System.Net.Http.Headers;
+    using System.Collections;
+    using System.Text;
+
+    /// <summary>
+    /// A WebApi Message handler to check authorization on incoming web requests
+    /// based on the OAuthAthorizationManger from the 
+    /// </summary>
+    public class OAuthAuthorizationHandler : DelegatingHandler
+    {
+        public OAuthAuthorizationHandler()
+        {
+        }
+
+        /// <summary>
+        /// Check if user is authenticated
+        /// If user is authenticated, check if user is authorized to perform action on requested resource
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected override System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            // Check if user is authenticated...
+            try
+            {
+                var principal = VerifyOAuth2(request);
+                                
+                if (principal != null)
+                {
+                    var policy = new OAuthPrincipalAuthorizationPolicy(principal);
+                    var policies = new List<IAuthorizationPolicy> {
+						policy,
+					};
+
+                    var securityContext = new ServiceSecurityContext(policies.AsReadOnly());
+                    if (operationContext.IncomingMessageProperties.Security != null)
+                    {
+                        operationContext.IncomingMessageProperties.Security.ServiceSecurityContext = securityContext;
+                    }
+                    else
+                    {
+                        operationContext.IncomingMessageProperties.Security = new SecurityMessageProperty
+                        {
+                            ServiceSecurityContext = securityContext,
+                        };
+                    }
+
+                    securityContext.AuthorizationContext.Properties["Identities"] = new List<IIdentity> {
+						principal.Identity,
+					};
+
+                    // Only allow this method call if the access token scope permits it.
+                    return principal.IsInRole();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (ProtocolException ex)
+            {
+                //Global.Logger.Error("Error processing OAuth messages.", ex);
+            }
+
+            return false;
+        }
+
+
+
+        private static IPrincipal VerifyOAuth2(HttpRequestMessage request)
+        {
+            // for this sample where the auth server and resource server are the same site,
+            // we use the same public/private key.
+            using (var signing = EncryptionConfig.CreateAuthorizationServerSigningServiceProvider())
+            {
+                using (var encrypting = EncryptionConfig.CreateResourceServerEncryptionServiceProvider())
+                {
+                    var resourceServer = new ResourceServer(new StandardAccessTokenAnalyzer(signing, encrypting));
+
+                    IPrincipal result;
+
+                    
+
+                    var error = resourceServer.VerifyAccess(
+                                                        HttpRequestInfo.Create( request.Method.Method, 
+                                                                                request.RequestUri, 
+                                                                                null, 
+                                                                                request.Headers.ToNameValueCollection()),
+                                                        out result);
+
+                    // TODO: return the prepared error code.
+                    return error != null ? null : result;
+                }
+            }
+        }
+
+    }
+}
